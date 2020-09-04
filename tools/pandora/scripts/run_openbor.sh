32beeb655228d9b11c217283db12f41a338bdc5c
Guillaume Gasnier <fantomid@free.fr>
2020-09-04 08:50:23 +0200

    Sync with branch pandora-v6391-dev: update scripts

--- tools/pandora/scripts/run_openbor.sh
+++ tools/pandora/scripts/run_openbor.sh
@@ -2,6 +2,7 @@
 
 # Check the version number to export variables for SDL1 or SDL2.
 VERSION=$1
+OPENBOR_MOD=$2
 SDL_VERSION="UNKNOWN"
 if [ "$VERSION" = "v3.0b3482" ] || [ "$VERSION" = "v3.0b3711" ] || [ "$VERSION" = "v3.0b3979" ] || [ "$VERSION" = "v3.0b4074" ] || [ "$VERSION" = "v3.0b4569" ]
 then
@@ -17,5 +18,5 @@ then
 fi
 if [ "$SDL_VERSION" != "UNKNOWN"  ]
 then
-  ./scripts/start_openbor.sh "$VERSION" "$SDL_VERSION"
+  ./scripts/start_openbor.sh "$VERSION" "$SDL_VERSION" "$OPENBOR_MOD"
 fi
